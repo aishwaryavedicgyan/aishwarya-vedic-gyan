74e82162f9b02b59acf9d96967e37c7155e1f5bd
krkundan15 <152591759+krkundan15@users.noreply.github.com>
2025-01-05 21:09:47 +0530

    commit

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
-import { Header } from './components/header/Header';
+import React from 'react';
+import { Header } from './components/Header/Header';
 import { Hero } from './components/Hero';
 import { Panchang } from './components/panchang/Panchang';
 import { Features } from './components/Features';
